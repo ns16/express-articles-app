25dd82fc4509ed48d74a6c198b1620a8f355db46
Nikolay Shamayko <nikolay.shamayko@gmail.com>
2023-08-30 20:23:06 +0700

    Created swagger doc

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import morgan from 'morgan'
+import swaggerUi from 'swagger-ui-express'
+import YAML from 'yamljs'
 
 import config from './config.js'
 import handleErrors from './lib/handle_errors.js'
 import router from './lib/router.js'
+
+const swaggerSchema = YAML.load('./swagger.yaml')
+swaggerSchema.servers = [
+  { url: `http://localhost:${config.port}`, description: 'Dev Server' }
+]
 
 const app = express()
 
@@ -16,6 +23,8 @@ if (process.env.NODE_ENV !== 'test') {
   app.use(morgan('dev'))
 }
 app.use('/api/v1', router())
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerSchema))
+app.use(express.static('static'))
 app.use(handleErrors)
 
 app.listen(process.env.NODE_ENV !== 'test' ? config.port : 9999, () => {
